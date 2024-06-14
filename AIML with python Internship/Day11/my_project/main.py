dd4c1f735bbf90e0928721b574b8616393211c8c
Shreyansh Jain <136733716+ShreyanshJain105@users.noreply.github.com>
2024-06-14 16:15:03 +0530

    Delete AIML with python Internship directory

--- AIML with python Internship/Day11/my_project/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-
-from package2.module2 import combined_hello
-
-def main():
-    print(combined_hello())
-
-if __name__ == "__main__":
-    main()
--- AIML with python Internship/Day11/my_project/package1/module1.py
+++
@@ -1,3 +0,0 @@
-# package1/module1.py
-def hello_from_module1():
-    return "Hello from module1 in package1!"
